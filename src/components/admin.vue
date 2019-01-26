fe21b20072bdbd0d4db0d99f332d73f517154b98
ZehaoChen <a15zehcheche@iam.cat>
2019-01-26 16:46:46 +0100

    problema de event de vue

--- src/components/admin.vue
+++ src/components/admin.vue
@@ -1,15 +1,14 @@
 <template>
   <div id="paleta_app">
-  <div class="admin">
-    <div class="paleta my-5 mx-5">
-    <a class="nav-link " @click="$router.push({ name: 'screenManager' })">Screen</a>
-    <a class="nav-link " @click="$router.push({ name: 'totemManager' })">Totem</a>
-    <a class="nav-link " @click="$router.push({ name: 'storeManager' })">Store</a>
-    </div>
-    <div class="workSpace  my-5 mx-5">
-      <router-view></router-view>
-    </div>
-  </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <a class="nav-link" @click="$router.push({ name: 'screenManager' })">Screen</a>
+        <a class="nav-link" @click="$router.push({ name: 'totemManager' })">Totem</a>
+        <a class="nav-link" @click="$router.push({ name: 'storeManager' })">Store</a>
+      </div>
+    </nav>
+    <br>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -26,33 +25,8 @@ export default {
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.admin{
-    display: flex;
-    justify-content: flex-start;
+.admin {
+  display: flex;
+  justify-content: flex-start;
 }
-
-.paleta{
-    width: 350px;
-    height: 700px;
-    border: 2px solid rgb(38, 145, 245);
-    border-radius: 5px;
-    box-sizing: border-box;
-}
-.paleta a{
- font-size: 2rem;   
-    padding: 20px 0;
-    border-bottom: 1px solid black;
-} 
-
-
-
-
-.workSpace{
-    width: 1400px;
-    height: 900px;
-    border: 2px solid rgb(38, 145, 245);
-    border-radius: 5px;
-    box-sizing: border-box; 
-}
-
 </style>
--- src/components/paleta/screenManager.vue
+++ src/components/paleta/screenManager.vue
@@ -1,13 +1,121 @@
 <template>
-    <div>screen</div>    
+  <div id="screenManager">
+    <div class="row">
+      <div class="col-2 sidebar">
+        <div class="sidenav">
+          <br>
+          <button type="button"  class="btn btn-primary boton">Add Colum</button>
+          <button type="button" class="btn btn-primary boton">Add row</button>
+          <div v-on:click="get_posision" row="0" column="0">prova</div>
+        </div>
+      </div>
+      <div v-html="html" class="col-10" id="show">{{html}}</div>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'paleta_app'
+export default {
+  name: "screenManager",
+
+  data() {
+    return {
+      msg:'hola',
+      html: null,
+      select: {
+        row:null,
+        column:null,
+      },
+      pantalla_layout: {
+        rows: [
+          {
+            columns: [
+              {
+                id: "store1",
+                text: "col 10",
+                width: 12,
+                color: "green",
+                row: 0,
+                col: 0
+              }
+            ]
+          }
+        ]
+      }
+    };
+  },
+  created() {
+    this.mount();
+  },
+  methods: {
+    get_posision(event) {
+      console.log(event.target); // Output : Select span|i|a element
+      this.select.row = event.target.getAttribute("row");
+      this.select.column = event.target.getAttribute("column");
+      console.log(this.select.row + ", "+ this.select.column ); 
+    },
+    createRow(r) {
+      return (
+        '<div class="row">' + '{{msg}}'+
+        this.parse(r) +
+        "</div>"
+      );
+    },
+    createColumn(c) {
+      return (
+        '<div v-on:click="get_posision" style="background-color : ' +
+        c.color +
+        "; height :400px" +
+        '" class="col-' +
+        c.width +
+        '" id="' +
+        c.id +
+         '" row="' +
+        c.row +
+        '" column="' +
+        c.col +
+        '">' +
+        (c.text ? c.text : "") +
+        this.parse(c) +
+        "</div>"
+      );
+    },
+    parse(s) {
+      let S = "";
+      if (s.rows) {
+        for (let i in s.rows) {
+          S += this.createRow(s.rows[i]);
+        }
+      }
+      if (s.columns) {
+        for (let i in s.columns) {
+          S += this.createColumn(s.columns[i]);
+        }
+      }
+      return S;
+    },
+    mount() {
+      this.html = this.parse(this.pantalla_layout);
+    },
+    addevent() {
+      $(".screen-row").attr('v-on:click'+'get_class');
+      $(".screen-column").html('v-on:click'+'get_class');
+    }
   }
+};
 </script>
 
 <style>
+.row {
+  height: 900px;
+  border: 1px solid #eee;
+}
+.sidebar {
+  border: 1px solid #eee;
+}
 
+.boton {
+  width: 200px;
+  margin: 10px;
+}
 </style>
